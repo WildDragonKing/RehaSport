dc7f650b2c054081462251ee4e00f726b2d3da8e
Lars <wilddragonking@googlemail.com>
2025-11-15 20:42:54 +0100

    Add browser Buffer polyfill (#20)

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Buffer } from "buffer";
 
 import App from "./App";
 import "./index.css";
+
+if (!globalThis.Buffer) {
+  (globalThis as typeof globalThis & { Buffer: typeof Buffer }).Buffer = Buffer;
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
