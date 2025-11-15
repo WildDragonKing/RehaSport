c0b1d51c62238e49f9f697debf63516947d17b44
Lars <wilddragonking@googlemail.com>
2025-11-15 20:46:13 +0100

    fix: initialize buffer polyfill before app (#21)

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,14 +1,10 @@
+import "./polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Buffer } from "buffer";
 
 import App from "./App";
 import "./index.css";
-
-if (!globalThis.Buffer) {
-  (globalThis as typeof globalThis & { Buffer: typeof Buffer }).Buffer = Buffer;
-}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
